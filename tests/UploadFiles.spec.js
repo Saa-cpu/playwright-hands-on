656210ebc4ec867ed433cfee5cf6f32687c5df49
Saajidh Ifthikar <mohamedsaajith90@gmail.com>
2025-05-14 12:02:14 +0530

    Multiple file upload and removal

--- tests/UploadFiles.spec.js
+++ tests/UploadFiles.spec.js
@@ -14,3 +14,23 @@ test.skip('Single File Uploads', async ({ page }) => {
 
 })
 
+test('Multiple File Uploads', async ({ page }) => {
+
+    await page.goto('https://davidwalsh.name/demo/multiple-file-upload.php');
+
+    await page.locator("#filesToUpload")
+    .setInputFiles(['tests/uploadfiles/test1.pdf','tests/uploadfiles/test2.pdf']);
+
+    await page.waitForTimeout(5000);
+
+    await expect(page.locator("div[class='main'] li:nth-child(1)")).toHaveText('test1.pdf');
+    await expect(page.locator("div[class='main'] li:nth-child(2)")).toHaveText('test2.pdf');
+
+    await page.waitForTimeout(3000);
+
+    await page.locator("#filesToUpload").setInputFiles([]);
+    await expect(page.locator("//li[normalize-space()='No Files Selected']")).toHaveText('No Files Selected');
+
+    await page.waitForTimeout(3000);
+})
+
