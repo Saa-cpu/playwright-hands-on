115688fc763cb67ed1cd4a000b95ff1fa7eab7c3
Saajidh Ifthikar <mohamedsaajith90@gmail.com>
2025-05-16 21:52:47 +0530

    Hooks test frame creation

---
+++ tests/Hooks.spec.js
@@ -0,0 +1,18 @@
+import {test, except} from '@playwright/test';
+
+test('Product Details', async ({page}) =>{
+
+    await page.goto('https://demoblaze.com/index.html');
+
+
+
+
+})
+
+
+test('Add to cart', async ({page}) =>{
+
+
+
+    
+})
