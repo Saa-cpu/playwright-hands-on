2918011cf52d1aed82d810954a95f72e2c6d417b
Saajidh Ifthikar <mohamedsaajith90@gmail.com>
2025-05-17 16:48:36 +0530

    Product details page

--- tests/Hooks.spec.js
+++ tests/Hooks.spec.js
@@ -1,18 +1,28 @@
-import {test, except} from '@playwright/test';
+import { test, except, expect } from '@playwright/test';
 
-test('Product Details', async ({page}) =>{
+test('Product Details', async ({ page }) => {
 
     await page.goto('https://demoblaze.com/index.html');
 
+    //click login button
+    await page.locator('#login2').click();
+    await page.locator('#loginusername').fill('pavanol')
+    await page.locator('#loginpassword').fill('test@123');
+    await page.locator("button[onclick='logIn()']").click();
 
+    //Get all products
+    const products = await page.$$('.hrefch');
+    expect(products).toHaveLength(9);
 
+    //Logging out
+    await page.locator('#logout2').click();
 
 })
 
 
-test('Add to cart', async ({page}) =>{
+test('Add to cart', async ({ page }) => {
 
 
 
-    
+
 })
