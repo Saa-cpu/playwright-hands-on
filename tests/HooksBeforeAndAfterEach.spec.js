d06ec6a78f66b7ba0c892cacb03a2b56d225ebe3
Saajidh Ifthikar <mohamedsaajith90@gmail.com>
2025-05-17 20:02:01 +0530

    Config changes and Hooks Each

---
+++ tests/HooksBeforeAndAfterEach.spec.js
@@ -0,0 +1,53 @@
+import { test, expect } from '@playwright/test';
+
+let page;
+
+test.beforeEach(async ({browser}) => {
+
+    page = await browser.newPage();
+    await page.goto('https://demoblaze.com/index.html');
+
+    //click login button
+    await page.locator('#login2').click();
+    await page.locator('#loginusername').fill('pavanol');
+    await page.locator('#loginpassword').fill('test@123');
+    await page.locator("button[onclick='logIn()']").click();
+
+})
+
+test.afterEach(async ()=>{
+
+    //Logging out
+    await page.locator('#logout2').click();
+    
+})
+
+
+test('Home page', async () => {
+
+    //Get all products
+    const products = await page.$$('.hrefch');
+    expect(products).toHaveLength(9);
+
+    await page.waitForTimeout(4000);
+
+})
+
+
+test('Add to cart', async () => {
+
+    //Click the product and add to cart
+    await page.locator("//a[normalize-space()='Samsung galaxy s6']").click();
+    await page.locator("//a[normalize-space()='Add to cart']").click();
+
+    //Handling the alert windown
+    page.on('dialog', async dialog => {
+        expect(dialog.type()).toContain('alert');
+        expect(dialog.message()).toContain('Product added.');
+        await dialog.accept();
+
+    })
+
+    await page.waitForTimeout(4000);
+
+})
