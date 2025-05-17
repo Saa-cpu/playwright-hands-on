26314878df2a5fb8d9d1e9b8bb28848c0fd53ef1
Saajidh Ifthikar <mohamedsaajith90@gmail.com>
2025-05-17 17:03:32 +0530

    Adding to cart feature

--- tests/Hooks.spec.js
+++ tests/Hooks.spec.js
@@ -1,4 +1,4 @@
-import { test, except, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
 test('Product Details', async ({ page }) => {
 
@@ -14,6 +14,8 @@ test('Product Details', async ({ page }) => {
     const products = await page.$$('.hrefch');
     expect(products).toHaveLength(9);
 
+    await page.waitForTimeout(4000);
+
     //Logging out
     await page.locator('#logout2').click();
 
@@ -22,7 +24,30 @@ test('Product Details', async ({ page }) => {
 
 test('Add to cart', async ({ page }) => {
 
+    await page.goto('https://demoblaze.com/index.html');
 
+    //click login button
+    await page.locator('#login2').click();
+    await page.locator('#loginusername').fill('pavanol')
+    await page.locator('#loginpassword').fill('test@123');
+    await page.locator("button[onclick='logIn()']").click();
+
+    //Click the product and add to cart
+    await page.locator("//a[normalize-space()='Samsung galaxy s6']").click();
+    await page.locator("//a[normalize-space()='Add to cart']").click();
+
+    //Handling the alert windown
+    page.on('dialog', async dialog => {
+        expect(dialog.type()).toContain('alert');
+        expect(dialog.message()).toContain('Product added.');
+        await dialog.accept();
+
+    })
+
+    await page.waitForTimeout(4000);
+
+    //Logging out
+    await page.locator('#logout2').click();
 
 
 })
