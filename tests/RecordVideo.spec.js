e28b30598a5b6e45c3e2e2dd4d90b32ddcf54846
Saajidh Ifthikar <mohamedsaajith90@gmail.com>
2025-05-20 17:42:49 +0530

    record videos

---
+++ tests/RecordVideo.spec.js
@@ -0,0 +1,16 @@
+import {test,expect} from '@playwright/test';
+
+test('Video record', async ({page})=>{
+
+    await page.goto('https://demoblaze.com/index.html');
+
+    //click login button
+    await page.locator('#login2').click();
+    await page.locator('#loginusername').fill('pavanol');
+    await page.locator('#loginpassword').fill('test@123');
+    await page.locator("button[onclick='logIn()']").click();
+    await expect(page.locator('#logout')).toBeVisible();
+
+
+    //check the congig files to see the screenshot and video configs
+})
