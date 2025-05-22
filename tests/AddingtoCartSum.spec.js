bda44d3dc0c329fda4f5ee6b88c9b75593dfbce8
Saajidh Ifthikar <mohamedsaajith90@gmail.com>
2025-05-22 15:37:22 +0530

    Improvements and comments to Add cart page

--- tests/AddingtoCartSum.spec.js
+++ tests/AddingtoCartSum.spec.js
@@ -3,20 +3,17 @@ import { log } from 'console';
 
 test('Add to cart and sum', async ({ page }) => {
 
-    await page.goto('https://www.saucedemo.com/');
-
-    await page.locator('#user-name').fill('standard_user');
-
+    await page.goto('https://www.saucedemo.com/'); // The login steps
+    await page.locator('#user-name').fill('standard_user')
     await page.locator('#password').fill('secret_sauce');
-
     await page.locator('#login-button').click();
 
-    await page.locator('#add-to-cart-sauce-labs-backpack').click();
+    await page.locator('#add-to-cart-sauce-labs-backpack').click(); //Clicking add to cart
     await page.locator('#add-to-cart-sauce-labs-bolt-t-shirt').click();
 
-    await page.locator('.shopping_cart_link').click();
+    await page.locator('.shopping_cart_link').click(); //Clicking the shop cart
 
-    const pricesInCart = await page.$$('.inventory_item_price');
+    const pricesInCart = await page.$$('.inventory_item_price'); //Getting all the item prices in the cart
 
     let sum = 0;
     for (let price of pricesInCart) {
@@ -41,9 +38,9 @@ test('Add to cart and sum', async ({ page }) => {
     const filteredSubTotal = subTotal.replace(/[^0-9.]/g, '').trim();
     const clean = parseFloat(filteredSubTotal)
 
-    await console.log('The filtered sub is ', clean);
+    console.log('The filtered sub is ', clean);
 
-    await expect(sum).toEqual(clean);
+    expect(sum).toEqual(clean);
 
 
 
