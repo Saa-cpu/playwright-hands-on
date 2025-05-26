7c6890f8e958fb1df73f13a1fe552bebb24c6b2e
Saajidh Ifthikar <mohamedsaajith90@gmail.com>
2025-05-26 18:26:02 +0530

    Annotation fixme and conditional skip

--- tests/Annotations.spec.js
+++ tests/Annotations.spec.js
@@ -4,3 +4,21 @@ test.only('test1', async ({ page }) => {
     console.log('This is test1')
 })
 
+test('test2', async ({ page }) => {
+    console.log('This is test2')
+})
+    
+
+//Conditional Skip
+test('test3', async ({ page, browserName }) => {
+    console.log('This is test3')
+    if (browserName === 'firefox') {
+        test.skip();
+    }
+})
+
+//Fixme
+test('test4', async ({ page }) => {
+    test.fixme();
+    console.log('This is test4')
+})
