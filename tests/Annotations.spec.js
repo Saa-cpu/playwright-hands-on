85e079c6fa26e1023683721b7d51c3bbb5d0ee5d
Saajidh Ifthikar <mohamedsaajith90@gmail.com>
2025-05-26 19:35:38 +0530

    Conditional fail

--- tests/Annotations.spec.js
+++ tests/Annotations.spec.js
@@ -24,11 +24,22 @@ test('test4', async ({ page }) => {
     console.log('This is test4')
 })
 
-
 //Fail annotation
 test('test5', async ({ page }) => {
     test.fail() //Makes the test fail
     console.log('This is test5')
     expect(1).toBe(1); //in normal cases this will pass since we need this failed it will fail
     //expect(1).toBe(2); //Here this is failing we want the test to fail therefore overall this TC will pass
+})
+
+//Conditional fail annotation
+//In the below test 
+test('test6', async ({ page, browserName}) => {
+    console.log('This is test5')
+    if(browserName === 'chromium') //pass
+    {
+        test.fail() //Makes the test fail  
+    }
+    //pass + fail = fail
+    //fail + fail = pass
 })
