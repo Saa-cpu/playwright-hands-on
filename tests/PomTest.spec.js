c1a7bae81ba1cde674baa7a9d3c865dbb024500b
Saajidh Ifthikar <mohamedsaajith90@gmail.com>
2025-06-08 19:05:20 +0530

    setting up pom

---
+++ tests/PomTest.spec.js
@@ -0,0 +1,11 @@
+import {test, expect} from '@playwright/test';
+
+test('test', async ({page}) =>{
+
+    //without pom
+    await page.goto('https://demoblaze.com/index.html');
+    await page.locator('#login2').click();
+    await page.locator('#loginusername').fill('pavanol');
+    await page.locator('#loginpassword').fill('test@123');
+    await page.locator("//button[normalize-space()='Log in']").click();
+})
