2b92f1cd3324b7fede0b24d9e60345c4ca8c84bb
Saajidh Ifthikar <mohamedsaajith90@gmail.com>
2025-06-09 21:18:29 +0530

    add product to cart method

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -1,13 +1,36 @@
+const { expect } = require("@playwright/test");
+exports.HomePage =
+    class HomePage {
 
-class HomePage {
+        constructor(page) {
+            this.page = page;
+            this.productList = "//div[@id='tbodyid']/div/div/div/h4/a"; //returns all the products
+            this.addToCartButton = '.btn.btn-success.btn-lg';
+            this.cartLinkButton = '#cartur';
+        }
 
-    constructor(page) {
-        this.page = page;
-        this.productList = "//div[@id='tbodyid']/div/div/div/h4/a"; //returns all the products
-        this.addToCartButton = '.btn.btn-success.btn-lg';
-        this.cartLinkButton = '#cartur';
+        async addProductToCart(productName) {
+            const prodList = await this.page.$$(this.productList);
+            for (const product of prodList) {
+                if (productName === await product.textContent()) {
+                    await product.click();
+                    break;
+                }
+            }
+
+            await this.page.on('dialog', async dialog => {
+                expect(dialog.message()).toContain('Product added.');
+                await dialog.accept();
+            })
+
+            await this.page.locator(this.addToCartButton).click();
+
+        }
+
+        async gotoCart() {
+            await this.page.locator(this.cartLinkButton).click();
+        }
+
+
+
     }
-
-
-
-}
