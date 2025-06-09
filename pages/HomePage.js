1bcca830318c9a4f5b2fbfab555294305d3d03c5
Saajidh Ifthikar <mohamedsaajith90@gmail.com>
2025-06-09 20:59:20 +0530

    add to cart page locators

---
+++ pages/HomePage.js
@@ -0,0 +1,13 @@
+
+class HomePage {
+
+    constructor(page) {
+        this.page = page;
+        this.productList = "//div[@id='tbodyid']/div/div/div/h4/a"; //returns all the products
+        this.addToCartButton = '.btn.btn-success.btn-lg';
+        this.cartLinkButton = '#cartur';
+    }
+
+
+
+}
--- tests/PomTest.spec.js
+++ tests/PomTest.spec.js
@@ -4,14 +4,6 @@ import {LoginPage} from '../pages/LoginPage';
 
 test('test', async ({page}) =>{
 
-    //without pom
-   // await page.goto('https://demoblaze.com/index.html');
-   // await page.locator('#login2').click();
-   // await page.locator('#loginusername').fill('pavanol');
-   // await page.locator('#loginpassword').fill('test@123');
-   // await page.locator("//button[normalize-space()='Log in']").click();
-
-
     //Login
     const loginTest = new LoginPage(page);
     await loginTest.gotoLoginPage();
