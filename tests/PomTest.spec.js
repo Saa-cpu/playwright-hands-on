70347fef9f3f905e83ad7b978b7f556eb9750594
Saajidh Ifthikar <mohamedsaajith90@gmail.com>
2025-06-10 15:53:55 +0530

    Cart page

--- tests/PomTest.spec.js
+++ tests/PomTest.spec.js
@@ -1,6 +1,7 @@
 import {test, expect} from '@playwright/test';
 import {LoginPage} from '../pages/LoginPage';
 import {HomePage} from '../pages/HomePage';
+import { CartPage } from '../pages/CartPage';
 
 
 test('test', async ({page}) =>{
@@ -13,14 +14,13 @@ test('test', async ({page}) =>{
     //Home page activities
     const Home = new HomePage(page);
     await Home.addProductToCart('Nexus 6');
+    await page.waitForTimeout(3000);
     await Home.gotoCart();
 
-  
+    //Cart page activities
+    const cart = new CartPage(page);
+    await page.waitForTimeout(3000);
+    const cartStatus = await cart.checkProductInCart('Nexus 6');
+    expect(cartStatus).toBe(true);
 
-
-
-    //Home
-
-
-    //Cart
 })
